c8db496fd0e86ea43131906c89bb8a3e0858a283
渣渣120 <WOSHIZHAZHA120@qq.com>
2021-08-19 17:25:06 +0800

    更新代理地址
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Services/Game/ProxyService.php
+++ app/Services/Game/ProxyService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 
 class ProxyService
 {
-    public string $proxy_url = 'http://localhost:10809';
+    public string $proxy_url = 'http://localhost:60007';
     public string $geometry_dash_base_url = 'http://www.boomlings.com/database';
 
     public function getProxyInstance(): PendingRequest
