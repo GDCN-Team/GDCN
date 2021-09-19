701fde55ad1dfc70649b65009d4dd3c6ff3773cd
渣渣120 <WOSHIZHAZHA120@qq.com>
2021-09-19 17:03:30 +0800

    优化代码 新增检测恶魔数量作弊
    Signed-off-by: 渣渣120 <WOSHIZHAZHA120@qq.com>

--- app/Services/Game/AntiCheatService.php
+++ app/Services/Game/AntiCheatService.php
@@ -15,7 +15,8 @@ class AntiCheatService
     public function run(): array
     {
         return [
-            $this->checkPlayerStars()
+            $this->checkPlayerStars(),
+            $this->checkPlayerDemons()
         ];
     }
 
@@ -24,22 +25,22 @@ class AntiCheatService
         $ratedStars = Rating::sum('stars');
         $mapPackStars = Pack::sum('stars');
         $gauntletStars = Gauntlet::all()
-            ->map(function (Gauntlet $gauntlet) {
-                return array_sum([
+            ->map(
+                fn(Gauntlet $gauntlet) => array_sum([
                     $gauntlet->getRelationValue('level1')?->rating?->stars ?? 0,
                     $gauntlet->getRelationValue('level2')?->rating?->stars ?? 0,
                     $gauntlet->getRelationValue('level3')?->rating?->stars ?? 0,
                     $gauntlet->getRelationValue('level4')?->rating?->stars ?? 0,
                     $gauntlet->getRelationValue('level5')?->rating?->stars ?? 0
-                ]);
-            })->sum();
+                ])
+            )->sum();
 
         $totalStars = array_sum([$baseStars, $ratedStars, $mapPackStars, $gauntletStars]);
         $cheatPlayers = UserScore::where('stars', '>', $totalStars)
             ->get()
-            ->map(function (UserScore $score) {
-                return $score->getRelationValue('user');
-            });
+            ->map(
+                fn(UserScore $score) => $score->getRelationValue('user')
+            );
 
         foreach ($cheatPlayers as $cheatPlayer) {
             BannedUser::create([
@@ -52,6 +53,34 @@ class AntiCheatService
                 ->info("[AntiCheat System AutoBan:Stars] Action: Banned", [
                     'user' => $cheatPlayer->id,
                     'stars' => $cheatPlayer->score->stars
+                ]);
+        }
+
+        return true;
+    }
+
+    public function checkPlayerDemons(int $baseDemons = 3): bool
+    {
+        $levelDemons = Rating::whereDemon(true)->count();
+        $totalDemons = array_sum([$baseDemons, $levelDemons]);
+
+        $cheatPlayers = UserScore::where('demons', '>', $totalDemons)
+            ->get()
+            ->map(
+                fn(UserScore $score) => $score->getRelationValue('user')
+            );
+
+        foreach ($cheatPlayers as $cheatPlayer) {
+            BannedUser::create([
+                'type' => BanType::BAN,
+                'user' => $cheatPlayer->id,
+                'reason' => 'AntiCheat: AntiBan:Demons'
+            ]);
+
+            Log::channel('gdcn')
+                ->info("[AntiCheat System AutoBan:Stars] Action: Banned", [
+                    'user' => $cheatPlayer->id,
+                    'demons' => $cheatPlayer->score->demons
                 ]);
         }
 
